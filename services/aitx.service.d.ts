e904f8f4515241c1c90e6526051b4e09d9ee29de
dhanashri.p <dhanashri@intelehealth.org>
2025-06-18 11:45:54 +0530

    build - v1.0.3

--- services/aitx.service.d.ts
+++ services/aitx.service.d.ts
@@ -1,15 +1,19 @@
 import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 import * as i0 from "@angular/core";
 export declare class AiTxService {
     private http;
     private env?;
+    private lastDiagnosis;
+    private cachedResponse;
     constructor(http: HttpClient, env?: any);
-    getAITTx(casehistory: any, diagnosis: any): import("rxjs").Observable<Object>;
+    getAITTx(casehistory: any, diagnosis: any): Observable<any>;
     getTxPayload(patientInfo: any, visit: any): string;
     getDataToExtract(patientInfo: any, visit: any): {};
     flatten(obj?: {}, parentKey?: string): {};
     formatText(text: string): string;
     markdownit(txt: any): any;
+    clearCache(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<AiTxService, [null, { optional: true; }]>;
     static ɵprov: i0.ɵɵInjectableDeclaration<AiTxService>;
 }
